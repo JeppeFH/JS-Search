bc0a6112a944597f591251b162f0157c31bc4e75
JeppeFH <jeppefinch@hotmail.dk>
2024-09-02 10:24:16 +0200

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,30 @@
-export function search() {}
+import { fetchProducts } from "./fetch_data.js";
+import { productListTmpl } from "./templates.js";
+
+const products = await fetchProducts();
+const productContainer = document.querySelector(".productContainer");
+
+export function search() {
+  /* Udskrive vores søgeresultater  */
+  function renderResult(result) {}
+
+  productContainer.innerHTML = "";
+
+  result.forEach((element) => {
+    productContainer.insertAdjacentHTML("beforeend", productListTmpl);
+  });
+
+  /* Finder de produkter der lever op til søgekriterierne */
+  function searchInput(event) {
+    const searchTerm = event.target.value;
+
+    const result = products.filter((product) =>
+      product.title.includes(searchTerm)
+    );
+
+    renderResult(result);
+  }
+
+  const input = document.querySelector("#search");
+  input.addEventListener("input", searchInput);
+}
