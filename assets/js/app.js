8616f7a3da5cda9da7298fdbe566d488aa4851f3
JeppeFH <jeppefinch@hotmail.dk>
2024-09-02 09:22:18 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
+import { productsList } from "./products_list.js";
+import { productsPage } from "./products_page.js";
+import { search } from "./search.js";
 
+const app = {};
+
+app.init = async () => {
+  productsList();
+  productsPage();
+  search();
+};
+
+app.init();
